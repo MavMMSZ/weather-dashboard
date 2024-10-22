9666e9df3c5f1cd1ad0bb6ea2d9fceb57f58e966
Maverick S <maverickmmsc@gmail.com>
2024-10-22 18:33:18 -0400

    its good enough

--- Develop/server/src/service/weatherService.ts
+++ Develop/server/src/service/weatherService.ts
@@ -1,4 +1,7 @@
+import dayjs from 'dayjs';
 import dotenv from 'dotenv';
+
+
 dotenv.config();
 // TODO: Define an interface for the Coordinates object
 interface Coordinates {
@@ -21,14 +24,14 @@ class Weather {
     tempF: string,
     windSpeed: string,
     humidity: string,) {
-      this.city = city
-      this.date = date;
-      this.icon = icon;
-      this.iconDescription = iconDescription;
-      this.tempF = tempF;
-      this.windSpeed = windSpeed;
-      this.humidity = humidity
-    }
+    this.city = city
+    this.date = date;
+    this.icon = icon;
+    this.iconDescription = iconDescription;
+    this.tempF = tempF;
+    this.windSpeed = windSpeed;
+    this.humidity = humidity
+  }
 }
 // TODO: Complete the WeatherService class
 class WeatherService {
@@ -39,10 +42,10 @@ class WeatherService {
     baseURL: string,
     APIkey: string,
     cityName: string,) {
-      this.baseURL = baseURL;
-      this.APIkey = APIkey;
-      this.cityName = cityName
-    }
+    this.baseURL = baseURL;
+    this.APIkey = APIkey;
+    this.cityName = cityName
+  }
   // TODO: Define the baseURL, API key, and city name properties
   // TODO: Create fetchLocationData method
   private async fetchLocationData(query: string) {
@@ -51,54 +54,56 @@ class WeatherService {
   }
   // TODO: Create destructureLocationData method
   private destructureLocationData(locationData: any): Coordinates {
-    const lat= locationData[0].lat;
-    const lon= locationData[0].lon;
-    return {lat, lon}
+    const lat = locationData[0].lat;
+    const lon = locationData[0].lon;
+    return { lat, lon }
   }
   // TODO: Create buildGeocodeQuery method
   private buildGeocodeQuery(): string {
-return `http://api.openweathermap.org/geo/1.0/direct?q=${this.cityName}&limit=1&appid=${this.APIkey}`;
+    return `http://api.openweathermap.org/geo/1.0/direct?q=${this.cityName}&limit=1&appid=${this.APIkey}`;
   }
   // TODO: Create buildWeatherQuery method
   private buildWeatherQuery(coordinates: Coordinates): string {
-    return `https://api.openweathermap.org/data/2.5/forecast?lat=${coordinates.lat}&lon=${coordinates.lon}&units=imperial&exclude=hourly&appid=${this.APIkey}`
+    return `https://api.openweathermap.org/data/2.5/forecast?lat=${coordinates.lat}&lon=${coordinates.lon}&units=imperial&appid=${this.APIkey}`
   }
   // TODO: Create fetchAndDestructureLocationData method
   private async fetchAndDestructureLocationData(): Promise<Coordinates> {
     const geocodeQuery = this.buildGeocodeQuery();
     console.log(geocodeQuery)
-  const locationData = await this.fetchLocationData(geocodeQuery);
-  const locationJson = await locationData.json();
-  console.log(locationJson)
-  const coordinates = this.destructureLocationData(locationJson);
-  return coordinates;
+    const locationData = await this.fetchLocationData(geocodeQuery);
+    const locationJson = await locationData.json();
+    console.log(locationJson)
+    const coordinates = this.destructureLocationData(locationJson);
+    return coordinates;
   }
   // TODO: Create fetchWeatherData method
   private async fetchWeatherData(coordinates: Coordinates) {
-  const weatherQuery = this.buildWeatherQuery(coordinates);
-  const weatherResponse = await fetch(weatherQuery);
-  return weatherResponse.json();
+    const weatherQuery = this.buildWeatherQuery(coordinates);
+    const weatherResponse = await fetch(weatherQuery);
+    return weatherResponse.json();
   }
   // TODO: Build parseCurrentWeather method
   private parseCurrentWeather(response: any): Weather {
     const currentWeather = response.list[0];
     const weather = new Weather(
-    this.cityName,
-    currentWeather.dt_txt,
-    currentWeather.weather[0].icon,
-    currentWeather.weather[0].description,
-    currentWeather.main.temp,
-    currentWeather.wind.speed.toString(),
-    currentWeather.main.humidity.toString()
-  );
-  return weather;
+      this.cityName,
+      currentWeather.dt_txt,
+      currentWeather.weather[0].icon,
+      currentWeather.weather[0].description,
+      currentWeather.main.temp,
+      currentWeather.wind.speed.toString(),
+      currentWeather.main.humidity.toString()
+    );
+    return weather;
   }
   // TODO: Complete buildForecastArray method
-   private buildForecastArray(currentWeather: Weather, weatherData: any[]): Weather[] {
+  private buildForecastArray(currentWeather: Weather, weatherData: any[]): Weather[] {
     const forecastArray = [currentWeather];
-    weatherData.filter((_, index) => index % 8 === 0).slice(0,5).forEach((forecast) => {
+
+    weatherData.filter((_, index) => index % 8 === 0).slice(0, 5).forEach((forecast) => {
       const weather = new Weather(
         this.cityName,
+        //dayjs(forecast.dt_txt).format('dddd')
         forecast.dt_txt,
         forecast.weather[0].icon,
         forecast.weather[0].description,
@@ -108,17 +113,17 @@ return `http://api.openweathermap.org/geo/1.0/direct?q=${this.cityName}&limit=1&
       );
       forecastArray.push(weather);
     });
-    console.log(weatherData)
+    console.log(forecastArray)
     return forecastArray;
-   }
+  }
   // TODO: Complete getWeatherForCity method
   async getWeatherForCity(city: string): Promise<Weather[]> {
     this.cityName = city;
-  const coordinates = await this.fetchAndDestructureLocationData();
-  const weatherData = await this.fetchWeatherData(coordinates);
-  const currentWeather = this.parseCurrentWeather(weatherData);
-  const forecastArray = this.buildForecastArray(currentWeather, weatherData.list);
-  return forecastArray;
+    const coordinates = await this.fetchAndDestructureLocationData();
+    const weatherData = await this.fetchWeatherData(coordinates);
+    const currentWeather = this.parseCurrentWeather(weatherData);
+    const forecastArray = this.buildForecastArray(currentWeather, weatherData.list);
+    return forecastArray;
   }
 }
 const baseURL = 'http://api.openweathermap.org';
